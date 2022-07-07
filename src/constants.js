6df558bf9f8d39cbde85e0d832cc948dd3695981
阳九 <871921282@qq.com>
2022-07-07 12:08:15 +0800

    first commit

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const name = '张三'
+export const age = 25
+export const sex = '男'
+export const role = '学生'
---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const path = require('path')
+module.exports = {
+    mode: 'production', // development|production
+    rootPath: __dirname, // 项目根路径
+    entry: path.join(__dirname, '/src/index.js'), //配置打包入口
+    output: path.join(__dirname, '/dist'), // 出口
+    hot: true,// 启动热更新
+}
