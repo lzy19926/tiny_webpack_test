bbaabac79ffb32294d291ae7cd64c40d33f15a9c
阳九 <871921282@qq.com>
2022-10-03 23:34:16 +0800

    feat: concurrent时间切片基本实现

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import App from './tinyReact_V2_test/App.lzy'
+
+//TODO 开启Demo测试页面
+// import Demo from './tinyReact_V2_test/Demo.lzy'
+
+//TODO 开启Concurrent模式测试页面
+import Concurrent from './tinyReact_V2_test/Concurrent.lzy'
