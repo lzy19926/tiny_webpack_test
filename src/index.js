75e1763db843d7c112d7b74678c41ac6ac6deddd
阳九 <871921282@qq.com>
2022-10-06 14:41:07 +0800

    feat: 修改element和fiber转换为二叉树的顺序, 预备添加和删除节点的diff逻辑

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
+//TODO 开启测试页面
+import Test from './tinyReact_V2_test/testPage/index.lzy'
 //TODO 开启Demo测试页面
-// import Demo from './tinyReact_V2_test/Demo.lzy'
+// import Demo from './tinyReact_V2_test/Demo/Demo.lzy'
 
 //TODO 开启Concurrent模式测试页面
-import Concurrent from './tinyReact_V2_test/Concurrent.lzy'
+// import Concurrent from './tinyReact_V2_test/Concurrent/Concurrent.lzy'
+
+
+
+
