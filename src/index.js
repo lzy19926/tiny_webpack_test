497f94b315b8436d69f3b72c7a787462a523a743
阳九 <871921282@qq.com>
2022-10-13 17:43:49 +0800

    feat: 基本实现diff算法  优化框架代码结构 框架0.3版基本完成

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 
 //TODO 开启测试页面
-import Test from './tinyReact_V2_test/testPage/index.lzy'
+// import Test from './tinyReact_V2_test/testPage/index.lzy'
+
 //TODO 开启Demo测试页面
 // import Demo from './tinyReact_V2_test/Demo/Demo.lzy'
 
@@ -8,5 +9,8 @@ import Test from './tinyReact_V2_test/testPage/index.lzy'
 // import Concurrent from './tinyReact_V2_test/Concurrent/Concurrent.lzy'
 
 
+// import V3test from './tinyReact_V3_test/test.lzy'
 
+import V3Demo from './tinyReact_V3_test/Demo/Demo.lzy'
 
+// import V3Concurrent from './tinyReact_V3_test/Concurrent/Concurrent.lzy'
