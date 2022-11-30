f41c6edd3582e152c5f7adf62c8b390f381612d2
阳九 <871921282@qq.com>
2022-11-30 14:59:36 +0800

    feat: 整合devServer的启动热更新代码到memoFs系统中, 实现ws端口配配置

--- public/script/startServer.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    console.warn("热更新准备就绪,正在监听文件")
-    var bundleHash = ''
-    var ws = new WebSocket("ws://localhost:3001/");
-    ws.onmessage = function ({ data }) {
-        if (bundleHash && bundleHash !== data) return location.reload()
-        bundleHash = data
-    }
-})()
